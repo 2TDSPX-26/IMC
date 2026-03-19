3ba5a04f192f7a8336e2019114f2227fe9e965e9
Eric Brito <eric_brito@msn.com>
2026-03-19 19:09:06 -0300

    Navegação entre telas

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+// https://github.com/2TDSPX-26/IMC
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
@@ -5,13 +7,47 @@ import ResultScreen from './src/screens/ResultScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+export type RootStackParamList = {
+  Home: undefined;
+  Result: undefined;
+  //  {
+  //   name: string;
+  //   gender: 'male' | 'female';
+  //   weight: number;
+  //   height: number;
+  // };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HomeScreen />
-      <ResultScreen />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{
+            title: '',
+          }}
+        />
+        <Stack.Screen
+          name='Result'
+          component={ResultScreen}
+          options={{
+            headerShown: true,
+            headerTitle: '',
+            headerBackTitle: '',
+            headerShadowVisible: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
